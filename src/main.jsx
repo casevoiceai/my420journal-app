a2a2adc5fbcc79b3a645678e3fd33e4114954f98
casevoiceai <casevoice.ai@gmail.com>
2026-08-15 20:31:56 -0400

    Wire private direct route for The New Place

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import WhoTookMyLighter from './features/games/who-took-my-lighter/WhoTookMyLighter'
+import TheNewPlace from './features/games/the-new-place/TheNewPlace'
 
 const directGamePath = window.location.pathname.replace(/\/+$/, '')
-const RootExperience = directGamePath === '/games/who-took-my-lighter'
-  ? WhoTookMyLighter
-  : App
+const directExperiences = {
+  '/games/who-took-my-lighter': WhoTookMyLighter,
+  '/games/the-new-place': TheNewPlace,
+}
+const RootExperience = directExperiences[directGamePath] || App
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
