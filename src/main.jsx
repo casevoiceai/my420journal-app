44064718cdc2c62e12902bcc506e9c675eea04cf
casevoiceai <casevoice.ai@gmail.com>
2026-08-15 20:24:34 -0400

    Wire private direct route for Who Took My Lighter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import WhoTookMyLighter from './features/games/who-took-my-lighter/WhoTookMyLighter'
+
+const directGamePath = window.location.pathname.replace(/\/+$/, '')
+const RootExperience = directGamePath === '/games/who-took-my-lighter'
+  ? WhoTookMyLighter
+  : App
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RootExperience />
   </StrictMode>
 )
